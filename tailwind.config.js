b2c8b1ef1c96ed736a28c2dd9787b68fbb8e742f
Christopher Bloom <bloomcb@gmail.com>
2020-10-12 13:27:39 -0700

    Upgrade to Tailwind 1.9

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,8 @@ module.exports = {
   future: {
     removeDeprecatedGapUtilities: true,
     purgeLayersByDefault: true,
+    defaultLineHeights: true,
+    standardFontWeights: true,
   },
   purge: ["./src/**/*.html", "./src/**/*.js"],
   theme: {
