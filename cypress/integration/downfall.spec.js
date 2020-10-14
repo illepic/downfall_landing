509c380785ba9046bbaf88cf88f124f3a6c7caf3
Christopher Bloom <bloomcb@gmail.com>
2020-10-13 20:21:24 -0700

    Simplify cypress tests

--- cypress/integration/downfall.spec.js
+++ cypress/integration/downfall.spec.js
@@ -8,21 +8,19 @@ describe("Downfall tests", () => {
   });
 
   it("Opens Discord link", () => {
-    cy.get(".df-info__article.discord")
-      .as("discord")
-      .should('not.be.visible')
+    cy.get(".df-info__article.discord").as("discord").should("not.be.visible");
 
     cy.contains("Discord").click();
     cy.url().should("include", "/#discord");
 
-    cy.get("@discord", {timeout: 1500}).should('be.visible')
+    cy.get("@discord", { timeout: 1500 }).should("be.visible");
 
     cy.get("@discord")
       .contains("Join the <Downfall> Discord here")
-      .should('be.visible')
+      .should("be.visible");
 
     cy.get(".df-info__close").click();
 
-    cy.get("@discord", { timeout: 500 }).should('not.be.visible')
+    cy.get("@discord", { timeout: 500 }).should("not.be.visible");
   });
 });
