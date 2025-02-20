2340033814e286e388976c1da0c26e9c1176fd39
Christopher Bloom <bloomcb@gmail.com>
2025-02-20 10:55:30 -0800

    New cypress

---
+++ cypress.config.js
@@ -0,0 +1,10 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    projectId: "fh28pw",
+    viewportWidth: 1024,
+    viewportHeight: 768,
+    baseUrl: "http://localhost:1234",
+  },
+});
